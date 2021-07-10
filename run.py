6711f157916111a4747a900195eedcbc35a057bd
rollercoaster111 <58612720+rollercoaster111@users.noreply.github.com>
2021-07-10 15:07:53 -0500

    run.py

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
 # Python file meant to run the website through flask. 
+from flask import Flask, render_template
+app = Flask(__name__, static_url_path='')
+
+@app.route("/")
+def home():
+    return render_template('main.html')
+    
+if __name__ == '__main__':
+   app.run()
