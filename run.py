ac7158fb7f30799f9933c565c3b5b79afa0d65f1
rollercoaster111 <58612720+rollercoaster111@users.noreply.github.com>
2021-07-10 15:44:20 -0500

    run.py

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # Python file meant to run the website through flask. 
 from flask import Flask, render_template
-app = Flask(__name__, static_url_path='')
+app = Flask(__name__, images_url_path='')
 
 @app.route("/")
 def home():
